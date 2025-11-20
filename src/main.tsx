7c800463cca0a525ac717b34dee9828944387a6f
KENOx7 <123300766+KENOx7@users.noreply.github.com>
2025-11-21 01:39:50 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
-// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// YENİ: ThemeProvider-i burada import edirik
-import { ThemeProvider } from "./context/theme-provider.tsx"; 
+import { ThemeProvider } from "./context/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    {/* ThemeProvider-i App komponentinin xaricinə, 
-      bura yerləşdirmək daha doğrudur.
-    */}
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  /* StrictMode silindi - OAuth xətalarını qarşısını almaq üçün */
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <App />
+  </ThemeProvider>
 );
